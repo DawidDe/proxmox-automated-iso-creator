7fc3624bf68f137eb796a10876fe3ef9d34f0459
Dawid Imbrzykowski <dawid.imb@gmail.com>
2025-07-21 23:09:58 +0200

    fixed curl command downloading iso

--- proxmox-auto-install-assistant.sh
+++ proxmox-auto-install-assistant.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -fsSL https://enterprise.proxmox.com/iso/proxmox-ve_8.4-1.iso /iso/proxmox.iso
+curl -fsSL https://enterprise.proxmox.com/iso/proxmox-ve_8.4-1.iso -o /iso/proxmox.iso
 
 proxmox-auto-install-assistant prepare-iso /iso/proxmox.iso --fetch-from iso --answer-file /iso/answer.toml
 
